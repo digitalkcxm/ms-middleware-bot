72935ce33e6f415d156bee5fb19ca43bd8a759d0
Rafael Mendes <rafaeldsilva16@gmail.com>
2023-01-16 18:10:10 -0300

    fix: ajustando o cache para ficar no ms

--- src/domain/session/createSession.js
+++ src/domain/session/createSession.js
@@ -7,7 +7,7 @@ const createSession = (
       assistantId: workspace,
     })
     .then((res) => {
-      redis.hset([`${projectName}:session:${protocol}`, res.result.session_id]);
+      redis.set([`${project_name}:session:${protocol}`, res.result.session_id]);
       return res.result.session_id;
     })
     .catch((err) => {
